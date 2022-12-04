d5fe8bf85bb1ee7638cc4021bd33c23054603a75
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2022-12-04 11:02:39 +0100

    Day 4 2022 1 star

--- src/day04/day4.cc
+++ src/day04/day4.cc
@@ -2,18 +2,44 @@
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 #include "paths.h"
-#include "utils/file_utils.h"
 
 #include <fmt/core.h>
 
-#include <string>
-#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <utility>
 
 int main()
 {
-    std::vector<std::string> input = ReadFile(DAY_4_INPUT);
-    for (const std::string &line : input)
-        fmt::print("{}\n", line);
+    std::ifstream in(DAY_4_INPUT);
 
+    std::pair<int, int> section_1;
+    std::pair<int, int> section_2;
+    unsigned int fully_contained_sections_count = 0;
+
+    while (!in.eof()) {
+        in >> section_1.first;
+        in.ignore(1);
+        in >> section_1.second;
+        in.ignore(1);
+        in >> section_2.first;
+        in.ignore(1);
+        in >> section_2.second;
+        in.ignore(1);
+
+        int smaller_min = std::min(section_1.first, section_2.first);
+        int bigger_max = std::max(section_1.second, section_2.second);
+
+        int union_max = bigger_max - smaller_min;
+        int biggest_section = std::max((section_1.second - section_1.first),
+                                       (section_2.second - section_2.first));
+
+        if (union_max <= biggest_section) ++fully_contained_sections_count;
+    }
+
+    fmt::print(
+        "The amount of section pairs that one is fully contained by the other "
+        "is : {}\n",
+        fully_contained_sections_count);
     return 0;
 }
