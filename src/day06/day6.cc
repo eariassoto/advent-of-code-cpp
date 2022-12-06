09fb4c6d61907c96bea66acf49aa2aae63771d0c
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2022-12-06 07:49:26 +0100

    Day 6 2022 1 star

--- src/day06/day6.cc
+++ src/day06/day6.cc
@@ -2,18 +2,38 @@
 // Use of this source code is governed by a MIT license that can be
 // found in the LICENSE file.
 #include "paths.h"
-#include "utils/file_utils.h"
 
 #include <fmt/core.h>
 
+#include <fstream>
+#include <unordered_set>
 #include <string>
-#include <vector>
 
 int main()
 {
-    std::vector<std::string> input = ReadFile(DAY_6_INPUT);
-    for (const std::string &line : input)
-        fmt::print("{}\n", line);
+    std::ifstream in(DAY_6_INPUT);
+    if (!in) return 1;
+
+    std::string input;
+    std::getline(in, input);
+
+    unsigned int marker_offset = 0;
+    std::unordered_set<char> marker_set;
+    for (int i = 0; i < input.size() - 3; ++i) {
+        marker_set.emplace(input[i]);
+        marker_set.emplace(input[i + 1]);
+        marker_set.emplace(input[i + 2]);
+        marker_set.emplace(input[i + 3]);
+        if (marker_set.size() == 4) {
+            marker_offset = i + 4;
+            break;
+        }
+        marker_set.clear();
+    }
+
+    fmt::print(
+        "Need to process {} new characters before the first start marker.\n",
+        marker_offset);
 
     return 0;
 }
