41cd84a8a5b77a0ad534da5c68f3faed60b6e111
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2022-12-07 15:04:17 +0100

    day 7 2022 1 star

--- src/day07/day7.cc
+++ src/day07/day7.cc
@@ -6,14 +6,153 @@
 
 #include <fmt/core.h>
 
+#include <algorithm>
+#include <cassert>
 #include <string>
 #include <vector>
+#include <utility>
+#include <queue>
+
+class Node
+{
+   public:
+    Node(const std::string& name) : name_(name), isFile_(false) {}
+
+    Node* GetParent() const
+    {
+        assert(!isFile_);
+        return parent_;
+    };
+
+    Node* GetChild(const std::string& name) const
+    {
+        assert(!isFile_);
+        auto it = std::find_if(children_.begin(), children_.end(),
+                               [&name](const Node* node) {
+                                   return !node->isFile_ && node->name_ == name;
+                               });
+        if (it != children_.end()) return *it;
+        return nullptr;
+    };
+
+    void AddFile(const std::string& name, unsigned int size)
+    {
+        Node* f = new Node();
+        f->name_ = name;
+        f->size_ = size;
+        f->isFile_ = true;
+        f->parent_ = this;
+        children_.push_back(f);
+    }
+
+    void AddDirectory(const std::string& name)
+    {
+        Node* f = new Node();
+        f->name_ = name;
+        f->isFile_ = false;
+        f->parent_ = this;
+        children_.push_back(f);
+    }
+
+    unsigned int GetSumAllUnder100k()
+    {
+        unsigned int result = 0;
+        GetSumAux(&result);
+        return result;
+    }
+    unsigned int GetTotalSum()
+    {
+        unsigned int result = 0;
+        return GetSumAux(&result);
+    }
+
+    static void DeleteNode(const Node* node)
+    {
+        std::queue<const Node*> to_delete;
+        to_delete.emplace(node);
+        while (!to_delete.empty()) {
+            for (const Node* n : to_delete.front()->children_) {
+                to_delete.emplace(n);
+            }
+            to_delete.pop();
+        }
+    }
+
+   private:
+    Node() = default;
+
+    unsigned int GetSumAux(unsigned int* sum)
+    {
+        // fmt::print("GetSumAux {}, sum: {}\n", name_, *sum);
+        unsigned int res = 0;
+        for (int i = 0; i < children_.size(); ++i) {
+            if (children_[i]->isFile_)
+                res += children_[i]->size_;
+            else
+                res += children_[i]->GetSumAux(sum);
+        }
+        if (res <= 100000) *sum += res;
+        // fmt::print("GetSumAux {}, sum is now: {}\n", name_, *sum);
+        return res;
+    }
+
+    std::string name_;
+    bool isFile_ = false;
+    unsigned int size_ = 0;
+
+    Node* parent_ = nullptr;
+    std::vector<Node*> children_;
+};
+
+using InputIt = std::vector<std::string>::iterator;
+
+void FillDirectory(Node* dir, InputIt& curr_command, const InputIt& command_end)
+{
+    ++curr_command;
+    while (curr_command != command_end && (*curr_command)[0] != '$') {
+        auto pos = (*curr_command).find(' ');
+        auto first_half = (*curr_command).substr(0, pos);
+        auto second_half = (*curr_command).substr(pos + 1);
+        if (first_half == "dir") {
+            // fmt::print("adding dir {}\n", second_half);
+            dir->AddDirectory(second_half);
+        } else {
+            // fmt::print("adding file {} size {}\n", second_half, first_half);
+            dir->AddFile(second_half, stoi(first_half));
+        }
+        ++curr_command;
+    }
+}
 
 int main()
 {
     std::vector<std::string> input = ReadFile(DAY_7_INPUT);
-    for (const std::string &line : input)
-        fmt::print("{}\n", line);
+
+    Node* root = new Node("/");
+    Node* aux = root;
+    InputIt curr_command = input.begin() + 1;
+
+    while (curr_command != input.end()) {
+        if ((*curr_command) == "$ ls") {
+            // fmt::print("found ls\n");
+            FillDirectory(aux, curr_command, input.end());
+        } else {
+            auto dir = (*curr_command).substr(5);
+            if (dir == "..") {
+                // fmt::print("cd to parent\n");
+                aux = aux->GetParent();
+            } else {
+                // fmt::print("cd to: {}\n", dir);
+                aux = aux->GetChild(dir);
+            }
+            ++curr_command;
+        }
+    }
+
+    unsigned int sum = root->GetSumAllUnder100k();
+    fmt::print("size sum of directories under 100k: {}\n", sum);
+
+    Node::DeleteNode(root);
 
     return 0;
 }
