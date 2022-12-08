c69a50b3cc9dec91b16621e3fc995ab4ecf11a28
Emmanuel Arias Soto <ariassotoemmanuel@gmail.com>
2022-12-08 18:54:04 +0100

    Day 8 2022 1 star

--- src/day08/day8.cc
+++ src/day08/day8.cc
@@ -8,12 +8,118 @@
 
 #include <string>
 #include <vector>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+
+struct pair_hash {
+    inline std::size_t operator()(const std::pair<int, int>& v) const
+    {
+        return v.first * 31 + v.second;
+    }
+};
 
 int main()
 {
     std::vector<std::string> input = ReadFile(DAY_8_INPUT);
-    for (const std::string &line : input)
-        fmt::print("{}\n", line);
+    // for (const std::string &line : input)
+    //     fmt::print("{}\n", line);
+
+    unsigned int map_width = input[0].size();
+    unsigned int map_heigth = input.size();
+
+    fmt::print("map w{}, h{}\n", map_width, map_heigth);
+
+    std::unordered_set<std::pair<int, int>, pair_hash> visible_trees;
+
+    // eval left
+    for (int h = 0; h < map_heigth; ++h) {
+        unsigned int aux = 0;
+        for (int w = 0; w < map_width; ++w) {
+            if (w == 0 || h == 0 || w == (map_width - 1) ||
+                h == (map_heigth - 1)) {
+                visible_trees.emplace(std::make_pair(w, h));
+            }
+            if (w == map_width - 1) {
+                continue;
+            }
+
+            const unsigned int out_tree = input[h][w] - '0';
+            aux = std::max(aux, out_tree);
+            const unsigned int in_tree = input[h][w + 1] - '0';
+            if (aux < in_tree) {
+                fmt::print("found {} {}: {}\n", w + 1, h, in_tree);
+                visible_trees.emplace(std::make_pair(w + 1, h));
+            }
+        }
+    }
+
+    // eval right
+    for (int h = 1; h < map_heigth; ++h) {
+        unsigned int aux = 0;
+        for (int w = map_width - 1; w >= 0; --w) {
+            if (w == 0 || h == 0 || w == (map_width - 1) ||
+                h == (map_heigth - 1)) {
+                visible_trees.emplace(std::make_pair(w, h));
+            }
+            if (w == 0) {
+                continue;
+            }
+
+            const unsigned int out_tree = input[h][w] - '0';
+            aux = std::max(aux, out_tree);
+            const unsigned int in_tree = input[h][w - 1] - '0';
+            if (aux < in_tree) {
+                fmt::print("found {} {}: {}\n", w - 1, h, in_tree);
+                visible_trees.emplace(std::make_pair(w - 1, h));
+            }
+        }
+    }
+
+    // eval top
+    for (int w = 0; w < map_width; ++w) {
+        unsigned int aux = 0;
+        for (int h = 0; h < map_heigth; ++h) {
+            if (w == 0 || h == 0 || w == (map_width - 1) ||
+                h == (map_heigth - 1)) {
+                visible_trees.emplace(std::make_pair(w, h));
+            }
+            if (h == map_heigth - 1) {
+                continue;
+            }
+
+            const unsigned int out_tree = input[h][w] - '0';
+            aux = std::max(aux, out_tree);
+            const unsigned int in_tree = input[h + 1][w] - '0';
+            if (aux < in_tree) {
+                fmt::print("found {} {}: {}\n", w, h + 1, input[h + 1][w]);
+                visible_trees.emplace(std::make_pair(w, h + 1));
+            }
+        }
+    }
+
+    // eval bottom
+    for (int w = 0; w < map_width; ++w) {
+        unsigned int aux = 0;
+        for (int h = map_heigth - 1; h >= 0; --h) {
+            if (w == 0 || h == 0 || w == (map_width - 1) ||
+                h == (map_heigth - 1)) {
+                visible_trees.emplace(std::make_pair(w, h));
+            }
+            if (h == 0) {
+                continue;
+            }
+
+            const unsigned int out_tree = input[h][w] - '0';
+            aux = std::max(aux, out_tree);
+            const unsigned int in_tree = input[h - 1][w] - '0';
+            if (aux < in_tree) {
+                fmt::print("found {} {}: {}\n", w, h - 1, input[h - 1][w]);
+                visible_trees.emplace(std::make_pair(w, h - 1));
+            }
+        }
+    }
+    fmt::print("{}\n", visible_trees.size());
 
     return 0;
 }
